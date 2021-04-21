88b2335c463f34d34eac92d00f4141751fc9f967
Crowdbotics <team@crowdbotics.com>
2021-04-21 01:04:59 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/carriesequel_25810/MainActivity.java
@@ -0,0 +1,15 @@
+package com.carriesequel_25810;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "carriesequel_25810";
+  }
+}
